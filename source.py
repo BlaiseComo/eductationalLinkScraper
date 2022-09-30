9f84b3c823b49a045883fbdccc25da2e50afbc0e
Blaise <comoblaise@gmail.com>
2022-09-30 11:49:26 -0600

    Currently, basic tik tok scraper

--- source.py
+++ source.py
@@ -2,25 +2,45 @@
 # - https://pandas.pydata.org/docs/user_guide/index.html#user-guide
 # - https://docs.python.org/3/library/re.html
 
+"""
 from bs4 import BeautifulSoup
 import requests
 
-websiteObject = requests.get('https://www.quora.com/Has-anyone-ever-tried-to-build-a-spaceship-of-their-own')
+websiteObject = requests.get('https://www.reddit.com/')
 
 if websiteObject.status_code >= 200 and websiteObject.status_code <= 400:
     print("Successful load!")
     print("Status Code: " + str(websiteObject.status_code))
 
-    soup = BeautifulSoup(websiteObject.content, "html.parser")
+    soup = BeautifulSoup(websiteObject.text, "html.parser")
 
-    listOfElements = ['h1', 'h2', 'h3', 'p', 'h4', 'li', 'span']
+    listOfElements = ['h1', 'h2', 'h3', 'p', 'h4', 'li']
 
     for i in listOfElements:
         pTags = soup.find_all(i)
 
         for x in range(1, len(pTags)):
             print(i + ": " + pTags[x].get_text())
-            print("\n\n\n")
+            print("\n")
 
 else:
-    print("Fuck you")
+    print("Connection Failed")
+"""
+
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
+
+PATH = "/home/blaise/Downloads/chromeDriver/chromedriver"
+
+driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+
+driver.get("https://www.tiktok.com/")
+
+search = driver.find_elements(By.TAG_NAME, 'strong')
+
+for i in search:
+    print(i.text)
+
+
