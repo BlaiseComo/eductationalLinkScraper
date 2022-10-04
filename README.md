363c95a8a2af8b70b69e3c07befdaec31df9f71b
Blaise <comoblaise@gmail.com>
2022-10-04 09:04:37 -0600

    Final Commit for V_1

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # webScraper
+This Programs functionality is limited to obtaining links from google scholar
+source.py is the file with the main code that runs this algorithm
+This program is able to obtain links using python and the selenium library
 This will be a repository holding all the files for my web scraper project
--- source.py
+++ source.py
@@ -8,39 +8,46 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.action_chains import ActionChains
 import time
 
-
+# WebScraper class is created for better organization
 class WebScraper:
     
-    def __init__(self):
+    # __init__ is defined in case it needs to be used in later versions
+    def __init__(self): 
         pass
 
+    # Function that obtains the links from the google scholar pages
     def getLinks(self, searchTerm, numberOfPages):
         
+        # webdriver object is initialized
         driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
 
         driver.get("https://scholar.google.com/")
 
+        # Search is set to the search bar on the google scholar page
         search = driver.find_element(By.ID, 'gs_hdr_tsi')
         
         search.send_keys(searchTerm)
         search.send_keys(Keys.RETURN)
 
+        # This variable is used to navigate through different pages
         count = 1
 
         listOfLinks = []
 
-        nextPageLink = ""
-
+        # For loop iterates through number of pages that user desires
         for page in range(0, numberOfPages):
-
+            
             try:
                 
+                # This statement makes sure that the page has loaded before it is searched
                 mainPage = WebDriverWait(driver, 10).until(
                     EC.presence_of_element_located((By.ID, 'gs_top'))
                 )
 
+                # The html code is obtained
                 pageToBeSearched = driver.page_source
 
+                # For loop iterates through the html code to look for links
                 for i in range(0, len(pageToBeSearched)):
 
                     if (pageToBeSearched[i:i+11] == 'href="https'):
@@ -62,21 +69,19 @@ class WebScraper:
 
             count += 1
 
+            # The next page is obtained through seleniums click functionality and the count variable
             nextPage = driver.find_element(By.LINK_TEXT, str(count))
             nextPage.click()
-
-            #nextPageLink = "/scholar?start=" + str(count) + "&q=" + searchTerm + "&h1=en&as_sdt=0,6"
-
-            #driver.get(nextPageLink)
 
 
         driver.quit()
 
         return listOfLinks
 
-    def obtainDataFromLinks(self, listOfLinks):
-        pass
 
+
+# Test code that can be uncommented to test program
+"""
 
 testObject = WebScraper()
 
@@ -85,5 +90,5 @@ listOfLinks = testObject.getLinks("astronomy", 2)
 for i in listOfLinks:
     print(i)
 
-
+"""
 
